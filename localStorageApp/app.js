26bfa654c22009609d52c383775db2dfbc0eafea
Marina Carvalho <marinacarvalho@protonmail.com>
2023-05-29 11:00:30 -0300

    using list instead of var
    and creating del all button

--- localStorageApp/app.js
+++ localStorageApp/app.js
@@ -1,12 +1,40 @@
 let nameField = document.getElementById('nameField');
-let nameDisplay = document.getElementById('nameDisplay');
 
-document.getElementById("nameForm").addEventListener("click", f);
+const subButton = document.getElementById('subButton');
+const delAllButton = document.getElementById('delAllButton');
+
+let employeesList = [];
+
+subButton.addEventListener("click", showName);
+delAllButton.addEventListener('click', delAll);
+
+function update() {
+	while (ulEmployees.firstChild) {
+		ulEmployees.removeChild(ulEmployees.firstChild)
+	}
+	
+	employeesList.forEach(e => {
+		liCreator(e)
+	});
+}
 
 function showName(e) {
 	e.preventDefault();
-	var t = nameField.value;
-	nameDisplay.innerHTML = t;
+	var name = nameField.value;
+
+	employeesList.push(name);
+	liCreator(name);
  }
 
- 
+ function delAll(e) {
+	e.preventDefault();
+	
+	employeesList = [];
+	update();
+ }
+
+ liCreator = (n) => {
+	let li = document.createElement('li');
+	li.innerText = n;
+	ulEmployees.appendChild(li);
+}
